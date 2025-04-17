e3747074a5bbf7913f52023f3160b7a89c58ed74
Egor-Shast-rezka <st129457@student.spbu.ru>
2025-04-17 15:33:59 +0300

    add load dictionary after create output file

--- Encoder_Shannon.cpp
+++ Encoder_Shannon.cpp
@@ -121,6 +121,7 @@ std::string CoderShannon::checkAnswerUser(std::string call, std::vector<std::str
 
 void CoderShannon::start_encoder() {
     
+    // Get user file
     map<char, float> freq;
     std::string file_name = checkAnswerUser("Write name input file (in format txt): ", {".txt"});
     std::string input = read_file(file_name);
@@ -146,17 +147,36 @@ void CoderShannon::start_encoder() {
 
     map<char, std::string> shannon_code_dict;
     for (const auto& pair : shannon_code) shannon_code_dict[pair.first] = pair.second;
-
-    std::string output_data;
-    for (char ch : input) output_data += shannon_code_dict[ch];
     
+    // Create output file
+    std::string output_data;
+    std::string dictionary_output_data;
+    for (char ch : input) output_data += shannon_code_dict[ch];
+    for (const auto& pair : shannon_code) {
+        if (std::string(1, pair.first) == "\n") {
+        
+            dictionary_output_data += "'\\n' : " + pair.second + "\n";
+            
+        } else dictionary_output_data += "'" + std::string(1, pair.first) + "' : " + pair.second + "\n";
+    }
+    
+    // Name for output file
     std::string output_file_name = "output.txt";
+    std::string dictionary_output_file_name = "dictionary.txt";
+    
+    // Writing in output file
     try {
+    
         write_file(output_file_name, output_data);
-        std::cout << "The file was recorded successfully!" << std::endl;
+        std::cout << "The file was recorded successfully!\n";
+        
+        write_file(dictionary_output_file_name, dictionary_output_data);
+        std::cout << "The file was recorded successfully!\n";
+        
     }
     catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+    
+        std::cerr << "Error: " << e.what() << "\n";
     }
 }
 
